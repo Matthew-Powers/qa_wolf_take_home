73a268d68aa4025fd48b7c286158045f6e8b4c85
Matthew Powers <mpowers16@ewu.edu>
2026-02-11 10:38:39 -0800

    Used PlayWrights record feature to grab some names/roles for the ycombinator website

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ async function sortHackerNewsArticles() {
   const page = await context.newPage();
 
   // go to Hacker News
-  await page.goto("https://news.ycombinator.com/newest");
+  await page.goto(('https://news.ycombinator.com/newest'));
+  await page.getByRole('link', { name: 'minutes ago' }).first().click();
+  await page.getByRole('link', { name: 'minutes ago' }).nth(1).click();
+  await page.getByRole('link', { name: 'More' }).click();
+  await page.getByRole('link', { name: '41 minutes ago' }).nth(2).click();
+  await page.getByRole('link', { name: 'More' }).click();
+  await page.getByRole('link', { name: 'More', exact: true }).click();
+  await page.locator('tr:nth-child(29) > .subtext > .subline > .age > a').click();
+  await page.locator('tr:nth-child(29) > .subtext > .subline > .age > a').click();
 }
 
 (async () => {
